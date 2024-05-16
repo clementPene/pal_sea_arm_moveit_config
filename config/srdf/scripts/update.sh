ff00db9c125ec65381eb1e93363b2bf4ec42ede9
Aina Irisarri <aina.irisarri@pal-robotics.com>
2024-05-16 10:41:59 +0200

    regenerate srdf disable collision files

--- config/srdf/scripts/update.sh
+++ config/srdf/scripts/update.sh
@@ -2,34 +2,53 @@
 set -e
 set -o pipefail
 ulimit -m 8048000
-pal_moveit_config_generator=$(rospack find pal_moveit_config_generator)
-source "$pal_moveit_config_generator/srdf_utils.sh" "$(dirname "${BASH_SOURCE[0]}")/../pal_sea_arm.srdf.xacro"
+pal_moveit_config_generator=$(ros2 pkg prefix pal_moveit_config_generator)
+source "$pal_moveit_config_generator/share/pal_moveit_config_generator/srdf_utils.sh" "$(dirname "${BASH_SOURCE[0]}")/../pal_sea_arm.srdf.xacro"
 
 # crawl all end effectors and generate the corresponding subtree SRDF
 for end_effector_file in "$srdf_folder"/end_effectors/*.srdf.xacro; do
     end_effector=$(basename "$end_effector_file" .srdf.xacro)
     echo $end_effector
-    for ft_sensor in false rokubi; do
-        args=(arm:=true "ft_sensor:=$ft_sensor" end_effector:="$end_effector")
-        if [ "$ft_sensor" != false ]; then
-            generate_disable_collisions_subtree arm_tool_link "${end_effector}_${ft_sensor}" "${end_effector}" "${args[@]}"
+    if [ "$end_effector" = "no-end-effector" ]; then
+        end_effector_value="no-ee"
+    else
+        end_effector_value="$end_effector"
+    fi
+    echo $end_effector_value
+
+
+    for ft_sensor in no-ft-sensor rokubi; do
+        args=(arm_type:='pal-sea-arm-standalone' "ft_sensor:=$ft_sensor" end_effector:="$end_effector")
+        if [ "$ft_sensor" != 'no-ft-sensor' ]; then
+            generate_disable_collisions_subtree arm_tool_link "${end_effector_value}_${ft_sensor}" "${end_effector_value}" "${args[@]}"
         else
-            generate_disable_collisions_subtree arm_tool_link "${end_effector}"  "" "${args[@]}"
+            generate_disable_collisions_subtree arm_tool_link "${end_effector_value}"  "" "${args[@]}"
         fi
     done
 done
 
 prefix="pal_sea_arm"
 
+generate_disable_collisions "${prefix}_no-ee" "${prefix}_no-arm" ft_sensor:="no-ft-sensor" end_effector:="no-end-effector" # plus arm
+generate_disable_collisions "${prefix}_no-ee_rokubi" "${prefix}_no-ee" ft_sensor:=rokubi end_effector:="no-end-effector" # plus FT sensor
+
 # crawl all end effectors and generate the corresponding SRDF
 for end_effector_file in "$srdf_folder"/end_effectors/*.srdf.xacro; do
     end_effector=$(basename "$end_effector_file" .srdf.xacro)
-    for ft_sensor in false rokubi; do
+    if [ "$end_effector" = "no-end-effector" ]; then
+        end_effector_value="no-ee"
+    else
+        end_effector_value="$end_effector"
+    fi
+    for ft_sensor in no-ft-sensor rokubi; do
+        echo $end_effector
         args=("ft_sensor:=$ft_sensor" end_effector:="$end_effector")
-        if [ "$ft_sensor" != false ]; then
-            generate_srdf "${prefix}_${end_effector}_${ft_sensor}" "${prefix}_no-ee_${ft_sensor}:${end_effector}_${ft_sensor}" "${args[@]}"
+        if [ "$ft_sensor" != 'no-ft-sensor' ]; then
+            echo "${prefix}_${end_effector_value}_${ft_sensor}" "${prefix}_no-ee_${ft_sensor}:${end_effector_value}_${ft_sensor}" "${args[@]}"
+            generate_srdf "${prefix}_${end_effector_value}_${ft_sensor}" "${prefix}_no-ee_${ft_sensor}:${end_effector_value}_${ft_sensor}" "${args[@]}"
         else
-            generate_srdf "${prefix}_${end_effector}" "${prefix}_no-ee:${end_effector}" "${args[@]}"
+            echo "${prefix}_${end_effector_value}" "${prefix}_no-ee:${end_effector_value}" "${args[@]}"
+            generate_srdf "${prefix}_${end_effector_value}" "${prefix}_no-ee:${end_effector_value}" "${args[@]}"
         fi
     done
 done
